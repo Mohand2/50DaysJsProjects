f7a46c7d635eacd260e0b6a083deae7900529cf3
Mohand2 <alloun.mohand@gmail.com>
2022-07-27 12:13:56 +0100

    Exapnding Cards v1

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const panels = document.querySelectorAll('.panel');
+
+function removeActiiveClass() {
+  panels.forEach((panel) => panel.classList.remove('active'));
+}
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiiveClass();
+    panel.classList.add('active');
+  });
+});
