6a540bbdb316d9f48cc0312816eeb5e7c8cc19b7
Mohand2 <alloun.mohand@gmail.com>
2022-07-28 00:00:56 +0100

    progress steps v2

--- progress_steps/scripts.js
+++ progress_steps/scripts.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const items = document.querySelectorAll('.item');
+const btnNext = document.querySelector('.btn-next');
+const btnBack = document.querySelector('.btn-back');
+
+const activateNextItem = () => {
+  for (let i = 0; i < items.length; i++) {
+    if (!items[i].classList.contains('active')) {
+      i === items.length - 1
+        ? btnNext.classList.add('btn-disabled')
+        : btnBack.classList.remove('btn-disabled');
+      items[i].classList.add('active');
+      break;
+    }
+  }
+};
+
+const activateBackItem = () => {
+  for (let i = items.length - 1; i >= 0; i--) {
+    if (i !== 0) {
+      if (items[i].classList.contains('active')) {
+        items[i].classList.remove('active');
+        i === 1
+          ? btnBack.classList.add('btn-disabled')
+          : btnNext.classList.remove('btn-disabled');
+
+        break;
+      }
+    }
+  }
+};
+
+btnNext.addEventListener('click', activateNextItem);
+btnBack.addEventListener('click', activateBackItem);
