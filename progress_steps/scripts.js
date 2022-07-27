fd40873375248416e96d42940c9a50eade0ad8b0
Mohand2 <alloun.mohand@gmail.com>
2022-07-28 00:11:12 +0100

    optimization

--- progress_steps/scripts.js
+++ progress_steps/scripts.js
@@ -17,16 +17,14 @@ const activateNextItem = () => {
 };
 
 const activateBackItem = () => {
-  for (let i = items.length - 1; i >= 0; i--) {
-    if (i !== 0) {
-      if (items[i].classList.contains('active')) {
-        items[i].classList.remove('active');
-        i === 1
-          ? btnBack.classList.add('btn-disabled')
-          : btnNext.classList.remove('btn-disabled');
+  for (let i = items.length - 1; i > 0; i--) {
+    if (items[i].classList.contains('active')) {
+      items[i].classList.remove('active');
+      i === 1
+        ? btnBack.classList.add('btn-disabled')
+        : btnNext.classList.remove('btn-disabled');
 
-        break;
-      }
+      break;
     }
   }
 };
