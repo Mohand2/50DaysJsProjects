54b83766b60c28b88810e3237d0278af26238b9a
Mohand2 <alloun.mohand@gmail.com>
2022-07-28 21:23:08 +0100

    added btn js

--- Rotating_navigation/script.js
+++ Rotating_navigation/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainArticle = document.querySelector('.container');
+const btn = document.querySelector('.btn');
+
+const rotateMainArticle = () => {
+  if (!mainArticle.classList.contains('rotate-container')) {
+    mainArticle.classList.add('rotate-container');
+    btn.classList.add('rotate-icons');
+  } else {
+    mainArticle.classList.remove('rotate-container');
+    btn.classList.remove('rotate-icons');
+  }
+};
+
+btn.addEventListener('click', rotateMainArticle);
