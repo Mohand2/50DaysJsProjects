89e686546be7e7eb7fb6cc7507c793ef80e4e399
Mohand2 <alloun.mohand@gmail.com>
2022-07-28 22:33:01 +0100

    Rotating navbar

--- Rotating_navigation/script.js
+++ Rotating_navigation/script.js
@@ -2,14 +2,17 @@
 
 const mainArticle = document.querySelector('.container');
 const btn = document.querySelector('.btn');
+const nav = document.querySelector('.nav');
 
 const rotateMainArticle = () => {
   if (!mainArticle.classList.contains('rotate-container')) {
     mainArticle.classList.add('rotate-container');
     btn.classList.add('rotate-icons');
+    nav.classList.add('nav-transition');
   } else {
     mainArticle.classList.remove('rotate-container');
     btn.classList.remove('rotate-icons');
+    nav.classList.remove('nav-transition');
   }
 };
 
